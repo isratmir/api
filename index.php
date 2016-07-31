76cc6e53222e9f0c933fe2da9147a255446bb805
Rad <isratmir@yandex.com>
2016-07-31 14:19:16 +0600

    Updates

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
-<?php 
-       header('Content-Type: application/json');
+<?php
+
 	require __DIR__ . '/vendor/autoload.php';
 
 	use Illuminate\Database\Capsule\Manager as Capsule;
-/*
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-*/
+
 	$capsule = new Capsule();
 	$capsule->addConnection([
 	    'driver'    => 'mysql',
 	    'host'      => 'localhost',
 	    'database'  => 'api',
 	    'username'  => 'root',
-	    'password'  => '12345',
+	    'password'  => '',
 	    'charset'   => 'utf8',
 	    'collation' => 'utf8_unicode_ci',
 	    'prefix'    => '',
@@ -21,13 +18,16 @@ ini_set("display_errors", 1);
 	$capsule->setAsGlobal();
 	$capsule->bootEloquent();
 
-	$q='';
 	$req = explode('/', trim($_SERVER['REQUEST_URI']));
 	$method = $_SERVER['REQUEST_METHOD'];
 
 	if($req[1] === 'date'){
 		$date = new DateTime('NOW');
 		$q = $date->format('Y-m-d h:i:s');
+
+		$res = array('status'=>'success', 'result'=>$q);
+		header('Content-Type: application/json');
+		echo json_encode($res);
 	}
 	elseif($req[1]==='expenses'){
 		if($method==='PUT'){
@@ -35,12 +35,39 @@ ini_set("display_errors", 1);
 				['title'=>$req[2],'price'=>$req[3]]
 			);
 			$q = $insert;
+
+			$res = array('status'=>'success', 'result'=>$q);
+			header('Content-Type: acpplication/json');
+			echo json_encode($res);
 		}
 		elseif($method==='GET'){
 			$q = Capsule::table('expenses')->get();
+
+			$res = array('status'=>'success', 'result'=>$q);
+			header('Content-Type: application/json');
+			echo json_encode($res);
+		}
+		elseif ( $method === 'DELETE' ) {
+			$q = Capsule::table('expenses')->where('id', '=', $req[2])->delete();
+
+			$res = array('status'=>'success', 'result'=>$q);
+			header('Content-Type: application/json');
+			echo json_encode($res);
 		}
 	}
+	elseif ( empty($req[1]) ){
+?>
+		<form action="" method="put">
+			<input type="hidden" name="interface" value="true">
+			<input type="text" name="title" id="" placeholder="Title">
+			<input type="text" name="price" id="" placeholder="Price">
+			<input type="submit" value="Save">
+		</form>
+<?php
+	}
 
-	$res = array('status'=>'success', 'result'=>$q);
-
-	echo json_encode($res);
+if(isset($_GET['interface']) && $_GET['interface'] === 'true'){
+	$insert = Capsule::table('expenses')->insert(
+			['title'=>$_GET['title'],'price'=>$_GET['price']]
+	);
+}
